a7d91349b1b8a1af325dfab582d54b349deccd57
«Elena <leseee4ka@bk.ru>
2023-08-06 21:28:34 +0300

    ex29

---
+++ exe29/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+int[] GetMassiv(int count)
+{
+    int[] arr = new int[count];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = GetNumberFromUser($"Введите {i} элемент массива: ", "Ошибка! Введите целое число."); 
+    }
+    return arr;
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+void PrintArr(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length-1)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.Write($"{array[i]}");
+    }
+Console.Write("]");
+}
+PrintArr (GetMassiv(8));
