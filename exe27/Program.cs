8c5acaf3e92909e3a9e60c8976b5295bd549b0d6
«Elena <leseee4ka@bk.ru>
2023-08-06 20:59:51 +0300

    ex27

---
+++ exe27/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int number = GetNumberFromUser("Введите число: ", "Ошибка! Введите целое число."); 
+GetSum(number);
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+void GetSum (int num)
+{
+    if (num < 0)
+        num = num * -1;
+int sum = 0;
+int cif = 0;
+
+    while (num > 0)
+    {
+        cif = num % 10;
+        num = num / 10;
+        sum = sum + cif;
+    }
+Console.WriteLine($"{sum}");
+}
